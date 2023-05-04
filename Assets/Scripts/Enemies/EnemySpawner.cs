f2fc4b4a1f953156d944d9a22ea7b5630b839b5a
ChrisHuntCAGD <91855979+ChrisHuntCAGD@users.noreply.github.com>
2023-05-04 13:52:12 -0700

    EnemySpawner

--- Assets/Scripts/Enemies/EnemySpawner.cs
+++ Assets/Scripts/Enemies/EnemySpawner.cs
@@ -4,12 +4,8 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private float spawnDelay;
-    [SerializeField] private GameObject enemyToSpawn;
-
     //The ordinal directions around the EnemySpawner
     [SerializeField] private SpawnPoint[] spawnPoints = new SpawnPoint[8];
-    private Vector3 openPoint = new Vector3(0, 0, 0);
-    private bool anyOpen = true;
 
     private void OnBecameVisible()
     {
@@ -18,32 +14,19 @@ public class EnemySpawner : MonoBehaviour
 
     private IEnumerator Spawn()
     {
-        GameObject enemy = ObjectPooler.Instance.GetPooledObject(enemyToSpawn.tag);
-        FindOpenPoint();
-
-        if(enemy != null && anyOpen)
+        for(short c = 0; c < 8; c++)
         {
-            enemy.transform.position = transform.position + openPoint;
-            enemy.SetActive(true);
-        }
-
-        yield return new WaitForSeconds(spawnDelay);
-        StartCoroutine(Spawn());
-    }
-
-    private void FindOpenPoint()
-    {
-        for (short c = 0; c < 8; c++)
-        {
-            if (!spawnPoints[c].occupied)
+            if(!spawnPoints[c].occupied)
             {
-                spawnPoints[c].occupied = true;
-                openPoint = spawnPoints[c].transform.position;
-                return;
+                spawnPoints[c].SpawnEnemy();
+                yield return new WaitForSeconds(spawnDelay);
+            }
+            else
+            {
+                yield return new WaitForSeconds(spawnDelay / 10);
             }
         }
 
-        //This is currently immediately triggering before anything can spawn. Fix this first
-        anyOpen = false;
+        StartCoroutine(Spawn());
     }
 }
--- Assets/Scripts/Enemies/SpawnPoint.cs
+++ Assets/Scripts/Enemies/SpawnPoint.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 public class SpawnPoint : MonoBehaviour
 {
     public bool occupied = false;
-    private void OnTriggerEnter(Collider other)
+    [SerializeField] private EnemySpawner parentSpawner;
+    [SerializeField] private GameObject enemyToSpawn;
+
+    public void SpawnEnemy()
     {
-        if(other.gameObject.layer == 7)
-        {
-            occupied = true;
-        }
+        GameObject enemy = ObjectPooler.Instance.GetPooledObject(enemyToSpawn.tag);
+        enemy.transform.position = transform.position;
+        enemy.SetActive(true);
+        occupied = true;
     }
 
     private void OnTriggerExit(Collider other)
